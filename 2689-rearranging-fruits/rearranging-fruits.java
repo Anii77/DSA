7aaf0f12f26993fbc4b3f6e206d6cb5c7a04d444
Anii77 <145887557+Anii77@users.noreply.github.com>
2025-08-02 16:15:11 +0530

    Time: 40 ms (95.77%) | Memory: 64.7 MB (69.01%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.java
+++ 2689-rearranging-fruits/rearranging-fruits.java
@@ -1,40 +1,32 @@
 class Solution {
     public long minCost(int[] basket1, int[] basket2) {
-       Map<Integer,Integer> totalCounts=new HashMap<>();
-       for(int a:basket1) totalCounts.put(a,totalCounts.getOrDefault(a,0)+1);
-       for(int b:basket2) totalCounts.put(b,totalCounts.getOrDefault(b,0)+1);
+       Map<Integer,Integer> freq=new HashMap<>();
+       for(int a:basket1) freq.put(a,freq.getOrDefault(a,0)+1);
+       for(int b:basket2) freq.put(b,freq.getOrDefault(b,0)-1);
 
-       long minVal=Long.MAX_VALUE;
-       for(Map.Entry<Integer,Integer> entry:totalCounts.entrySet())
-       {
-        if(entry.getValue()%2!=0) return -1;
-        minVal=Math.min(minVal,entry.getKey());
-       } 
-       List<Long> fruits=new ArrayList<>();
-       Map<Integer,Integer> count1=new HashMap<>();
-       for(int i:basket1)
-       count1.put(i,count1.getOrDefault(i,0)+1);
+       List<Integer> extra=new ArrayList<>();
+       long minValue=Long.MAX_VALUE;
 
-       for(Map.Entry<Integer,Integer> entry:totalCounts.entrySet())
+       for(Map.Entry<Integer,Integer> entry:freq.entrySet())
        {
         int fruit=entry.getKey();
-        int diff=count1.getOrDefault(fruit,0)-(entry.getValue()/2);
-       
-        for(int i=0;i<Math.abs(diff);i++)
-        {
-            fruits.add((long)fruit);
-        }
-       }
-        Collections.sort(fruits);
+        int count=entry.getValue();
+        minValue=Math.min(minValue,fruit);
 
-        long total=0;
-        int swaps=fruits.size()/2;
-        for(int i=0;i<swaps;i++)
+        if(count%2!=0) return -1;
+        for(int i=0;i<Math.abs(count)/2;i++)
         {
-            total+=Math.min(fruits.get(i),2*minVal);
+            extra.add(fruit);
         }
-      
-       
-         return total;
+       } 
+       Collections.sort(extra);
+   long cost=0;
+   int n=extra.size();
+   for(int i=0;i<n/2;i++)
+   {
+    cost+=Math.min(extra.get(i),2*minValue);
+   }
+   return cost;
     }
+
 }
